c7288da5dca14a51d4ded6573db6ce5b3a50b02c
Supreme Cadet <azulayidan1@gmail.com>
2024-05-15 17:48:30 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express" 
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
