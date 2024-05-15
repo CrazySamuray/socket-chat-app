e5962239e33c88c6d436d34e2aaf764830d19673
Supreme Cadet <azulayidan1@gmail.com>
2024-05-15 19:04:27 +0300

    add backend

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,10 +2,10 @@ import express from 'express';
 import { signup, login, logout } from '../controllers/auth.controller.js';
 const router = express.Router();
 
-router.get('/signup', signup);
+router.post('/signup', signup);
 
-router.get('/login', login);
+router.post('/login', login);
 
-router.get('/logout', logout);
+router.post('/logout', logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 import express from "express" 
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import userRoutes from "./routes/user.routes.js";
+
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+dotenv.config();
 
-app.use('/api/auth', authRoutes)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// })
 
 app.listen(PORT, () => {
     connectToMongoDB()
