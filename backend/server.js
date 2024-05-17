e8cdb76ca1d7f61fb117b71bd4259444987a546d
Supreme Cadet <azulayidan1@gmail.com>
2024-05-17 13:41:45 +0300

    add production build scripts

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
@@ -20,9 +22,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
